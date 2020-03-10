aa85872a966b26f7a2fa043ab5f6c35f4169b29a
IntiR <yo@example.com>
2020-03-10 14:02:46 -0500

    prueba

---
+++ punto2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+  float n=0;
+  float sum=0;
+ 
+  for(n=1; n<=10; n++){
+
+    sum=sum + 1/n;
+    
+    cout << n<<"\t" << sum<<endl;
+
+
+  }
+  }
